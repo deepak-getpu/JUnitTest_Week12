15521218c260a12f9211a5bfd089e63cc076946e
deepak-getpu <deepak.getpu@gmail.com>
2024-02-17 22:30:55 -0500

    update

--- src/main/java/com/promineotech/TestDemo.java
+++ src/main/java/com/promineotech/TestDemo.java
@@ -2,11 +2,19 @@ package com.promineotech;
 
 public class TestDemo {
 	
-	public int addPostive(int a, int b) {
+	public static int addPositive(int a, int b) {
 	if(a > 0 && b > 0) {
 		return a + b;
 	} else 
 		throw new IllegalArgumentException("Both parameters must be positive.");
+	}
+	
+	public static boolean isEven(int a) {
+		if(a%2==0) { 
+			return true;
+		} else {
+			return false;
+		}		
 	}
 }
 
--- src/test/java/com/promineotech/TestDemoJUnitTest.java
+++ src/test/java/com/promineotech/TestDemoJUnitTest.java
@@ -1,19 +1,64 @@
 package com.promineotech;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class TestDemoJUnitTest {
+	
+	private TestDemo testDemo;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		testDemo = new TestDemo();
+	}
+
+	@ParameterizedTest
+	@MethodSource("com.promineotech.TestDemoJUnitTest#argumentsForAddPositive")
+	void assertThatTwoPositiveNumbersAreAddedCorrectly() {
+		int a;
+		int b;
+		int expected;
+		boolean expectException;
+		if(!expectException) {
+			assertThat(TestDemo.addPositive(a, b)).isEqualTo(expected);
+		} else {
+			assertThatThrownBy(() -> testDemo.addPositive(a, b)).isInstanceOf(IllegalArgumentException.class);
+		}
+		assertThat(testDemo.addPositive(4,5)).isEqualTo(9);
+		assertThat(testDemo.addPositive(6,7)).isEqualTo(13);
+	}
+
+	public static Stream<Object> argumentsForAddPositive() {
+		return Stream.of();
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void assertThatPairsOfPositiveNumbersAreAddedCorrectly() {
+		assertThat(testDemo.addPositive(40,50)).isEqualTo(90);	
+		assertThat(testDemo.addPositive(400,500)).isEqualTo(900);
 	}
-
+	
+	
+	@Test
+	void assertThatNumberIsEven() {
+		int a;
+		int expected;
+		boolean expectException;
+		if(!expectException) {
+			assertThat(TestDemo.isEven(a)).isEqualTo(expected);
+		} else {
+			assertThatThrownBy(() -> testDemo.isEven(a)).isInstanceOf(IllegalArgumentException.class);
+		}
+		assertThat(TestDemo.isEven(4)).isEqualTo(true);
+		assertThat(TestDemo.isEven(9)).isEqualTo(false);
+	}
+	
 }
